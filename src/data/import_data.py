a599801eff8f5ea322d315c2c796cc6d03e983dd
denncc <denn_c@outlook.com>
2021-01-12 17:07:07 -0800

    data importation pipeline finished

--- src/data/import_data.py
+++ src/data/import_data.py
@@ -16,5 +16,13 @@ def download_seq(path, f = "sam"):
     print(type(first_srr), first_srr)
     for i in range(50):
         srr = "SRR" + str(first_srr + i)
-        url = f"https://trace.ncbi.nlm.nih.gov/Traces/sra/?path=https%3A%2F%2Fsra-downloadb.be-md.ncbi.nlm.nih.gov%2Fsos1%2Fsra-pub-run-2%2F{srr}%2F{srr}.1&run={srr}&acc=CM000663.2&ref=chr1&range=&src=0&output=sam&output_to=File"
-        print(url)
+        # url = f"https://trace.ncbi.nlm.nih.gov/Traces/sra/?path=https%3A%2F%2Fsra-downloadb.be-md.ncbi.nlm.nih.gov%2Fsos1%2Fsra-pub-run-2%2F{srr}%2F{srr}.1&run={srr}&acc=CM000663.2&ref=chr1&range=&src=0&output=sam&output_to=File"
+        url_1 = f"ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR794/00{(4 + i) % 10}/SRR{7949794 + i}/SRR{7949794 + i}_1.fastq.gz"
+        url_2 = f"ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR794/00{(4 + i) % 10}/SRR{7949794 + i}/SRR{7949794 + i}_2.fastq.gz"
+        command_1 = f"wget {url_1} -P {path}"
+        command_2 = f"wget {url_2} -P {path}"
+        print(f"start downloading SRR{7949794 + i}_1")
+        os.system(command_1)
+        print(f"start downloading SRR{7949794 + i}_2")        
+        os.system(command_2)
+        print(f"finish downloading SRR{7949794 + i}")
