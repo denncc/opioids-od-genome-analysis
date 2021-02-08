96ee26e6778a694db2298ad41bc6269d31745306
denncc <denn_c@outlook.com>
2021-02-07 18:47:37 -0800

    finish run.py test

--- run.py
+++ run.py
@@ -17,13 +17,22 @@ with open("./config/feature_config.json") as f:
     feature_config = json.load(f)
 cts_dir = feature_config["cts_dir"]
 
+with open("./config/test_config.json") as f:
+    test_config = json.load(f)
+test_data_path = test_config["data"]
+test_kallisto_output = test_config["output"]
+
 def main(target):
     if target == "run.py":
         # import_data.download_seq(data_path)
         # import_data.convert_idx(kallisto_idx_input, kallisto_idx_output)
         import_data.align_kallisto(kallisto_idx_output, data_path, kallisto_output)
         # build_features.make_cts(kallisto_output, cts_dir)
-
+    if target == "test":
+        import_data.test_download_seq(test_data_path)
+        import_data.convert_idx(kallisto_idx_input, kallisto_idx_output)
+        import_data.test_align_kallisto(kallisto_idx_output, test_data_path, test_kallisto_output)
+        build_features.test_make_cts(test_kallisto_output, cts_dir)
 
 
 if __name__ == "__main__":
--- src/data/import_data.py
+++ src/data/import_data.py
@@ -14,6 +14,8 @@ def download_seq(path, f = "sam"):
     sample input: sam, bam, fastq, fasta
     """
     print(type(first_srr), first_srr)
+    if not os.path.exists(path):
+        os.makedirs(path)
     for i in range(50):
         srr = "SRR" + str(first_srr + i)
         # url = f"https://trace.ncbi.nlm.nih.gov/Traces/sra/?path=https%3A%2F%2Fsra-downloadb.be-md.ncbi.nlm.nih.gov%2Fsos1%2Fsra-pub-run-2%2F{srr}%2F{srr}.1&run={srr}&acc=CM000663.2&ref=chr1&range=&src=0&output=sam&output_to=File"
@@ -51,7 +53,7 @@ def align_kallisto(kallisto_idx_dir, datadir, output):
             os.makedirs(outdir)
         input1 = os.path.join(datadir, "SRR" + str(first_srr + i) + "_1.fastq.gz")
         input2 = os.path.join(datadir, "SRR" + str(first_srr + i) + "_2.fastq.gz")
-        command = f'/opt/kallisto_linux-v0.42.4/kallisto quant -i {kallisto_idx_dir} -o {outdir}/out.bam --pseudobam {input1} {input2}'
+        command = f'/opt/kallisto_linux-v0.42.4/kallisto quant -i {kallisto_idx_dir} -o {outdir} --pseudobam {input1} {input2}'
         print(command)
         # os.system(command)
     return 
@@ -62,3 +64,45 @@ def remove_duplicates():
     """
     for i in range(50):
         sample_dir = os.path.join()
+
+def test_download_seq(path, f = "sam"):
+    """
+    The pipeline to call linux wget to get all dataset into
+    the specified locaiton on DSMLP in the specified format
+    path: a string of the absolute path on DSMLP
+    format: a string of the format of the sequence, 
+    sample input: sam, bam, fastq, fasta
+    """
+    print(type(first_srr), first_srr)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for i in range(1):
+        srr = "SRR" + str(first_srr + i)
+        # url = f"https://trace.ncbi.nlm.nih.gov/Traces/sra/?path=https%3A%2F%2Fsra-downloadb.be-md.ncbi.nlm.nih.gov%2Fsos1%2Fsra-pub-run-2%2F{srr}%2F{srr}.1&run={srr}&acc=CM000663.2&ref=chr1&range=&src=0&output=sam&output_to=File"
+        url_1 = f"ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR794/00{(4 + i) % 10}/SRR{7949794 + i}/SRR{7949794 + i}_1.fastq.gz"
+        url_2 = f"ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR794/00{(4 + i) % 10}/SRR{7949794 + i}/SRR{7949794 + i}_2.fastq.gz"
+        command_1 = f"wget {url_1} -P {path}"
+        command_2 = f"wget {url_2} -P {path}"
+        print(f"start downloading SRR{7949794 + i}_1")
+        os.system(command_1)
+        print(f"start downloading SRR{7949794 + i}_2")        
+        os.system(command_2)
+        print(f"finish downloading SRR{7949794 + i}")
+        return
+
+def test_align_kallisto(kallisto_idx_dir, datadir, output):
+    """
+    The pipeline that aligns the _1 and _2 gene
+    """
+    if not os.path.exists(output):
+        os.makedirs(output)
+    for i in range(1):
+        outdir = os.path.join(output, "SRR" + str(first_srr + i))
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        input1 = os.path.join(datadir, "SRR" + str(first_srr + i) + "_1.fastq.gz")
+        input2 = os.path.join(datadir, "SRR" + str(first_srr + i) + "_2.fastq.gz")
+        command = f'/opt/kallisto_linux-v0.44.0/kallisto quant -i {kallisto_idx_dir} -o {outdir} --pseudobam {input1} {input2}'
+        print(command)
+        os.system(command)
+    return 
