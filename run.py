c47feada5ad92a1c052cbda121051332b49e0ace
denncc <denn_c@outlook.com>
2021-03-07 17:34:39 -0800

    pipeline finished

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import sys
 # import the modules under src
 import src.data.import_data as import_data
 import src.features.build_features as build_features
+import src.models.build_model as build_model
+import src.models.htseq_cts as htseq_cts
 
 with open("./config/data_config.json") as f:
     config = json.load(f)
@@ -22,12 +24,41 @@ with open("./config/test_config.json") as f:
 test_data_path = test_config["data"]
 test_kallisto_output = test_config["output"]
 
+with open("./config/model_config.json") as f:
+    model_config = json.load(f)
+samtools_sh_path = model_config["samtools_sh"]
+htseq_output = model_config["htseq_output"]
+deseq = model_config["deseq"]
+wgcna = model_config["wgcna"]
+
 def main(target):
     if target == "run.py":
+        # download the data from NCBI
         # import_data.download_seq(data_path)
+
+        # Convert the Kallisto index
         # import_data.convert_idx(kallisto_idx_input, kallisto_idx_output)
-        import_data.align_kallisto(kallisto_idx_output, data_path, kallisto_output)
-        build_features.make_cts(kallisto_output, cts_dir)
+
+        # Kallisto Alignment
+        # import_data.align_kallisto(kallisto_idx_output, data_path, kallisto_output)
+
+        # Read counts of Kallisto Alignments
+        # build_features.make_cts(kallisto_output, cts_dir)
+
+        # Samtools preprocessing
+        # build_model.run_shell(samtools_sh_path)
+
+        # HTSeq counts rendering
+        # htseq_cts.cts(htseq_output)
+        
+        # DESeq2
+        build_model.run_R(deseq)
+
+        # WGCNA
+        build_model.run_R(wgcna)
+        print("finished the whole pipeline")
+        
+
     if target == "test":
         import_data.test_download_seq(test_data_path)
         import_data.convert_idx(kallisto_idx_input, kallisto_idx_output)
